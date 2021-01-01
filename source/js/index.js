373632fcf83e4d1581c9d557143d1d6ee3858d12
Semyon <saymon221090@gmail.com>
2021-01-01 20:46:06 +0300

    подключает приложение к основной странице

--- source/js/index.js
+++ source/js/index.js
@@ -1,1 +1,9 @@
 import "../scss/main.scss";
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./pages/main.jsx";
+
+ReactDOM.render(
+    <Main/>,
+    document.getElementById(`root`)
+);
