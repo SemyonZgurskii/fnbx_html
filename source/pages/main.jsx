e0b6922550741137e8b4cf85cb825ad6671c2226
Semyon <saymon221090@gmail.com>
2021-01-04 10:56:33 +0300

    исправляет баг IE с min-height

--- source/pages/main.jsx
+++ source/pages/main.jsx
@@ -4,12 +4,14 @@ import Products from "../components/products/products.jsx";
 export default function Main(props) {
   return (
     <main className="main">
-      <section className="main__content">
-        <p className="main__text">Ты сегодня покормил кота?</p>
-        <div className="main__products">
-          <Products {...props}/>
-        </div>
-      </section>
+      <div className="main__wrapper">
+        <section className="main__content">
+          <p className="main__text">Ты сегодня покормил кота?</p>
+          <div className="main__products">
+            <Products {...props}/>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
