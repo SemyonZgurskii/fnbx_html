8fd25063ea7bf8cded0bb4b262cb81b4e31af639
Semyon <saymon221090@gmail.com>
2021-01-05 16:37:39 +0300

    редактирует конфиг под текущий проект

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  rootDir: `./src`,
-  transform: {
-    "^.+\\.tsx?$": `ts-jest`,
-    "^.+\\.js?$": `babel-jest`,
-  },
-  testRegex: `.test.(js?|jsx?|tsx?)$`,
-  moduleFileExtensions: [
-    `ts`,
-    `tsx`,
-    `js`,
-    `jsx`,
-    `json`,
-    `node`
-  ],
+  rootDir: `./source`,
 };
